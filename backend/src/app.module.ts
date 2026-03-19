cf2acbb1c7694edb903c3b9acb8b25f54179b0ba
Ronald Jesus <ronaldrioscondori85@gmail.com>
2026-03-18 19:46:32 -0500

    feat: crear entidad Proveedor y establecer relacion uno-a-muchos con Producto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ProveedoresModule } from './proveedores/proveedores.module';
 
 @Module({
-  imports: [ProductosModule],
+  imports: [ProductosModule, ProveedoresModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
